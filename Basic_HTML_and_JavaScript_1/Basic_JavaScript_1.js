07c21be0a43a6588fa1da62305c012976005cad7
Arieleon25 <Arieleon25@yahoo.com>
2023-01-30 17:08:01 -0500

    Creating a Function part 2
    Added function to JavaScript
    Incorporated Java's code to Basic_HTML_2

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,7 @@
 alert("I am an alert box!");
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML = 
+		result;
+	}
